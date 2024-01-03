80e696dffbffdd5286263f9a1b1ad65a727e4bd2
OmerSalomon <102198073+OmerSalomon@users.noreply.github.com>
2024-01-03 22:15:41 +0200

    plus minus scanning and replacing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import math
 
+
 def get_single_operand_operators_list():
     single_operand_operators = ['!', '~']
     return single_operand_operators
+
 
 def get_operator_dict():
     my_dict = {
@@ -19,6 +21,7 @@ def get_operator_dict():
     }
 
     return my_dict
+
 
 def double_operand_culc(val_1: float, val_2: float, operator: chr) -> float:
     val_1 = float(val_1)
@@ -47,6 +50,7 @@ def double_operand_culc(val_1: float, val_2: float, operator: chr) -> float:
         else:
             return val_1
 
+
 def factorial(n: float) -> int:
     if n.is_integer() and n < 0:
         raise ValueError("There is no factorial for non native number")
@@ -56,13 +60,13 @@ def factorial(n: float) -> int:
         result *= i
     return result
 
+
 def single_operand_culc(val: float, operator: chr) -> float:
     val = float(val)
     if operator == '~':
         return -val
     elif operator == '!':
         return factorial(val)
-
 
 
 def pop_and_culc(values: list, operators: list):
@@ -77,7 +81,18 @@ def pop_and_culc(values: list, operators: list):
         result = double_operand_culc(val_1, val_2, op)
         return result
 
+
+def fix_plus_minus(s: str):
+    while ('++' in s) or ('--' in s) or ('-+' in s) or ('+-' in s):
+        s = s.replace('++', '+')
+        s = s.replace('--', '+')
+        s = s.replace('-+', '-')
+        s = s.replace('+-', '-')
+
+    return s
+
 def calculate_exp(exp: str, op_dict: dict) -> float:
+    exp = fix_plus_minus(exp)
     values = []
     operators = []
 
@@ -103,8 +118,9 @@ def calculate_exp(exp: str, op_dict: dict) -> float:
 
     return values.pop()
 
+
 def main():
-    exp = '3*~(5!)'
+    exp = '33'
     result = calculate_exp(exp, get_operator_dict())
     print(result)
 

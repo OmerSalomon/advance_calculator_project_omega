20a3de6c78710f65dff980e1882eacfbc632136e
OmerSalomon <102198073+OmerSalomon@users.noreply.github.com>
2024-01-03 20:58:57 +0200

    factorial and negative added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
 
+def get_single_operand_operators_list():
+    single_operand_operators = ['!', '~']
+    return single_operand_operators
 
 def get_operator_dict():
     my_dict = {
@@ -10,12 +13,14 @@ def get_operator_dict():
         '^': 3,
         '@': 5,
         '&': 5,
+        '!': 6,
+        '~': 6,
         '(': 0
     }
 
     return my_dict
 
-def culc(val_1: float, val_2: float, operator: chr) -> float:
+def double_operand_culc(val_1: float, val_2: float, operator: chr) -> float:
     val_1 = float(val_1)
     val_2 = float(val_2)
 
@@ -42,16 +47,37 @@ def culc(val_1: float, val_2: float, operator: chr) -> float:
         else:
             return val_1
 
+def factorial(n: float) -> int:
+    if n.is_integer() and n < 0:
+        raise ValueError("There is no factorial for non native number")
+    n = int(n)
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
+def single_operand_culc(val: float, operator: chr) -> float:
+    val = float(val)
+    if operator == '~':
+        return -val
+    elif operator == '!':
+        return factorial(val)
+
+
 
 def pop_and_culc(values: list, operators: list):
     op = operators.pop()
-    val_1 = values.pop()
-    val_2 = values.pop()
-    result = culc(val_1, val_2, op)
-    return result
+    if op in get_single_operand_operators_list():
+        val = values.pop()
+        result = single_operand_culc(val, op)
+        return result
+    else:
+        val_1 = values.pop()
+        val_2 = values.pop()
+        result = double_operand_culc(val_1, val_2, op)
+        return result
 
-
-def calculate_exp(exp: str, op_dict: dict):
+def calculate_exp(exp: str, op_dict: dict) -> float:
     values = []
     operators = []
 
@@ -78,7 +104,7 @@ def calculate_exp(exp: str, op_dict: dict):
     return values.pop()
 
 def main():
-    exp = '9/3^2'
+    exp = '3*~(5!)'
     result = calculate_exp(exp, get_operator_dict())
     print(result)
 
